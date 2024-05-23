4e71539304b71e8db173bf4ce5b45d0f1b6dc882
Santi <santimartelli@gmail.com>
2024-05-23 02:58:45 +0200

    Empezada página de proyectos y agregados botones de redes sociales

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,22 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
-	},
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn .5s ease-in forwards',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0,  },
+          '100%': { opacity: 1, },
+        },
+      },
+    },
+  },
+	variants: {
+    extend: {
+      animation: ['hover', 'focus', 'motion-safe'],
+    },
+  },
 	plugins: [],
 }
